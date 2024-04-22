34d2d5f6b760be6dfd9524f14d38583fc6a3da9a
Phamducvinh <phamducvinh2k3@gmail.com>
2024-04-22 21:27:22 +0700

    tuan 2

---
+++ tuan1/src/main/java/resources/SimpleProgram.java
@@ -0,0 +1,24 @@
+package resources;
+
+public class SimpleProgram {
+    
+    public static void main(String[] args) {
+        SimpleProgram program = new SimpleProgram();
+        program.runProgram(5);
+    }
+    
+    public void runProgram(int number) {
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even");
+                } else {
+                    System.out.println(i + " is odd");
+                }
+            }
+        } else {
+            System.out.println("Number must be positive");
+        }
+    }
+}
+
